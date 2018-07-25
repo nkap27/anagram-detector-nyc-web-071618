5db31eea4e64cd308b9cc7227aaa0465b3df4331
github username <github email address>
2018-07-25 10:14:54 -0400

    final anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_arr)
+    anagrams = []
+
+    sorted_word =  @word.split("").sort
+
+    words_arr.each do |word|
+    	word_alpha = word.split("").sort
+      if word_alpha == sorted_word
+        anagrams << word
+      end
+    end
+
+    anagrams
+
+  end
+
+end
+
+#%w(foo bar) is a shortcut for ["foo", "bar"]
